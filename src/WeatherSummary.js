3030c0d732a95c2509a80fdb1ca8237cd479e85d
balaas324 <balaas324@gmail.com>
2021-04-29 17:54:44 +0200

    final

--- src/WeatherSummary.js
+++ src/WeatherSummary.js
@@ -1,23 +1,43 @@
 import React, {useState, useEffect} from 'react'
 import { WeatherEntry } from './WeatherEntry';
-import { readWeather } from './WeatherService';
+import { readForecast, readWeather } from './WeatherService';
+import './summary.scss'
 
 export const WeatherSummary = ({locationProp}) => {
     const [weather, setWeather] = useState(null);
+    const [forecast, setForecast] = useState(null)
 
     useEffect(() => {
-        if (locationProp) {
-            readWeather(locationProp.id).then(weather => setWeather(weather));
-        }
-    }, [locationProp]);
+        (async function () {
+          if (locationProp) {
+            const [weather, forecast] = await Promise.all([
+              readWeather(locationProp.id),
+              readForecast(locationProp.id)
+            ]);
+            setWeather(weather);
+            setForecast(forecast);
+          }
+        })();
+      }, [locationProp]);
 
-    if (!locationProp || !weather) return null;
+    if (!locationProp || !weather || !forecast) return null;
 
     return (
         <div>
             <hr/>
             <h2>{locationProp.name}</h2>
             <WeatherEntry weatherProp={weather}/>
+
+            <h2>Forecast</h2>
+            <div>
+                <ol style={({whiteSpace: 'nowrap'})}>
+                {forecast.map(timePoint =>
+                    <li key={timePoint.dt}>
+                    <WeatherEntry weatherProp={timePoint}/>
+                    </li>
+                )}
+                </ol>
+            </div>
         </div>
     );
 }
